9971066d38205b96f2a381850dddc0e00dffad3a
Daniel Salazar <daniel.esalazars@gmail.com>
2020-08-19 19:03:06 -0500

    arreglando menu flotante

--- static/js/guest.js
+++ static/js/guest.js
@@ -10,12 +10,24 @@ jQuery(document).ready(function($) {
     $(".gracias").toggleClass('d-none');
   })
   //click a menu float moviles y click a close moviles
-  $("#header button, #close").click(function(){
+  $("#header button").click(function(){
+      $("#menuFloat").toggleClass('d-none');
     //activando efecto de derecha a izquierda
     setTimeout(function () {
       $("#menuFloat").toggleClass('active');
       $("#menuFloat .contenedor").toggleClass('active');
     }, 300)
+  })
+
+  //click a menu float moviles y click a close moviles
+  $("#close").click(function(){
+    $("#menuFloat").toggleClass('active');
+    $("#menuFloat .contenedor").toggleClass('active');
+    //activando efecto de derecha a izquierda
+    setTimeout(function () {
+      $("#menuFloat").toggleClass('d-none');
+    }, 300)
+
   })
 
   // $(window).on("load", function () {
